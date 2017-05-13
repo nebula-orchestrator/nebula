0aa5ec47f31b52b5b329d8d5a6c844d9edfb32b0
naorlivne <naorlivne@gmail.com>
2017-05-13 20:47:25 +0300

    rabbit error massages breaking reqursive connect function

--- worker-manager/functions/rabbit_functions.py
+++ worker-manager/functions/rabbit_functions.py
@@ -1,87 +1,51 @@
-import pika, os
+import pika
 
 
 # connect to rabbit function
 def rabbit_connect(rabbit_user, rabbit_pass, rabbit_host, rabbit_port, rabbit_virtual_host):
-    try:
-        credentials = pika.PlainCredentials(rabbit_user, rabbit_pass)
-        connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host, rabbit_port, rabbit_virtual_host, credentials, heartbeat_interval=0))
-        return connection
-    except:
-        print "rabbit_connect issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    credentials = pika.PlainCredentials(rabbit_user, rabbit_pass)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host, rabbit_port, rabbit_virtual_host, credentials, heartbeat_interval=0))
+    return connection
 
 
 # close connection to rabbit function
 def rabbit_close(rabbit_connection):
-    try:
-        rabbit_connection.close()
-    except:
-        print "rabbit close issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    rabbit_connection.close()
 
 
 # create channel
 def rabbit_create_channel(rabbit_connection):
-    try:
-        channel = rabbit_connection.channel()
-        return channel
-    except:
-        print "rabbit create channel issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    channel = rabbit_connection.channel()
+    return channel
 
 
 # create exchange
 def rabbit_create_exchange(rabbit_channel, exchange_name):
-    try:
-        rabbit_channel.exchange_declare(exchange=exchange_name, type='fanout')
-    except:
-        print "rabbit create exchange issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    rabbit_channel.exchange_declare(exchange=exchange_name, type='fanout')
 
 
 # send message
 def rabbit_send(rabbit_channel, exchange_name, rabbit_send_message):
-    try:
-        rabbit_channel.basic_publish(exchange=exchange_name, routing_key='', body=rabbit_send_message)
-    except:
-        print "rabbit send issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    rabbit_channel.basic_publish(exchange=exchange_name, routing_key='', body=rabbit_send_message)
 
 
 # receive message
 def rabbit_receive(rabbit_receive_channel, rabbit_work_function, rabbit_receive_queue):
-    try:
-        rabbit_receive_channel.basic_consume(rabbit_work_function, queue=rabbit_receive_queue)
-        rabbit_receive_channel.start_consuming()
-    except:
-        print "rabbit receive issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    rabbit_receive_channel.basic_consume(rabbit_work_function, queue=rabbit_receive_queue)
+    rabbit_receive_channel.start_consuming()
 
 
 # ack message
 def rabbit_ack(rabbit_ack_channel, rabbit_ack_method):
-    try:
-        rabbit_ack_channel.basic_ack(delivery_tag=rabbit_ack_method.delivery_tag)
-    except:
-        print "rabbit ack issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    rabbit_ack_channel.basic_ack(delivery_tag=rabbit_ack_method.delivery_tag)
 
 
 # create queue
 def rabbit_create_queue(rabbit_queue_name, rabbit_channel):
-    try:
-        created_queue = rabbit_channel.queue_declare(queue=rabbit_queue_name, arguments={"x-expires": 300000})
-        return created_queue
-    except:
-        print "rabbit create queue issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    created_queue = rabbit_channel.queue_declare(queue=rabbit_queue_name, arguments={"x-expires": 300000})
+    return created_queue
 
 
 # bind queue to exchange
 def rabbit_bind_queue(rabbit_bind_queue_name, rabbit_bind_channel, rabbit_bind_exchange):
-    try:
-        rabbit_bind_channel.queue_bind(exchange=rabbit_bind_exchange, queue=rabbit_bind_queue_name)
-    except:
-        print "rabbit bind queue issue - dropping container due to rabbit connection issues"
-        os._exit(2)
+    rabbit_bind_channel.queue_bind(exchange=rabbit_bind_exchange, queue=rabbit_bind_queue_name)
